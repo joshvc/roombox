b8715db37237563c3f889b233bf3589c0f79e4e3
Josh Van Cleef <josh@joshvc.com>
2014-08-18 10:58:46 -0400

    add color to printed labels

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,12 @@
 class Room < ActiveRecord::Base
   has_many :boxes
   has_many :items, through: :boxes
+
+  def color_code
+    if color.blank?
+      "#000"
+    else
+      color.gsub("#","").insert(0, "#")
+    end
+  end
 end
