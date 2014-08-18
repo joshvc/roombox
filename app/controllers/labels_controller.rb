e9eb779bd69c28edce45eb2fcf7ebe8c412d1944
Josh Van Cleef <josh@joshvc.com>
2014-08-18 14:40:47 -0400

    revise labels controller

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -3,15 +3,26 @@ class LabelsController < ApplicationController
     password: Rails.application.secrets.http_basic_password
 
   def index
-    if params[:row].empty?
-      @empty_boxes = 0
-    else
-      @empty_boxes = (params[:row].to_i - 1) * 2
-    end
-    params[:to].empty? ? to = Box.last.id : to = params[:to].to_i
-    params[:from].empty? ? from = 1 : from = params[:from].to_i
-
-    @boxes = Box.where(id: from..to).to_a
+    @empty_boxes = empty_boxes(params)
+    @boxes = Box.where(id: first_box(params)..last_box(params)).to_a
     @first_boxes = @boxes.shift(10 - @empty_boxes)
   end
+
+private
+  def first_box(params)
+    params[:from].empty? ? from = 1 : from = params[:from].to_i
+  end
+
+  def last_box(params)
+    params[:to].empty? ? to = Box.last.id : to = params[:to].to_i
+  end
+
+  def empty_boxes(params)
+    if params[:row].empty?
+      0
+    else
+      (params[:row].to_i - 1) * 2
+    end
+  end
+
 end
